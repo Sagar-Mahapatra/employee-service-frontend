e59858408f5c8f7b878cd6bb470a241e32baea09
Sagar Mahapatra <sagarkmohapatra2012@gmail.com>
2022-04-08 13:54:40 +0530

    implmented code in profile ts,html,css

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -7,9 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  name!: string;
+  clickBtn = 'waves-effect waves-light btn-large';
+  btnColor = 'red';
+  btnApperence: boolean = true;
+  cars: string[] = ['Audi', 'Mercediz'];
+
+  constructor() {
+    console.log('ProfileComponent constructor executed...');
+    setTimeout(() => {
+      if (this.name == 'sagar') {
+        this.name = 'a developer';
+      } else {
+        this.name = 'sagar';
+      }
+    }, 2000);
+    const s = ['a developer', 'sagar'];
+    this.name = s[Math.floor(Math.random() * 2)];
+  }
 
   ngOnInit(): void {
   }
 
+  change() {
+    console.log('ProfileComponent change() executed...');
+    if (this.name == 'sagar') {
+      this.name = 'a developer';
+    } else {
+      this.name = 'sagar';
+    }
+
+    if (this.btnApperence == true) {
+      this.btnApperence = false;
+    } else {
+      this.btnApperence = true;
+    }
+  }
+
 }
+
